4fd05b3d8409e421a6498e377375e25b79ecb832
William Rios <133013480+Winnd11@users.noreply.github.com>
2024-06-22 16:01:21 -0300

    Add files via upload

---
+++ boolean/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace boolean;
+
+class Program
+{
+    static void Main(String[] args)
+    {
+        Console.WriteLine("how old are you?");
+        int ageOption = Convert.ToInt32(Console.ReadLine());
+
+        bool age = ageOption >= 18;
+
+        if (age == true)
+        {
+            Console.WriteLine("you are of legal age");
+        } else
+        {
+            Console.WriteLine("you are a minor");
+        }
+    }
+}

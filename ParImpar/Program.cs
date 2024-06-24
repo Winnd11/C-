16803ceb661c4095863fa4e61c690760364f0eee
William Rios <133013480+Winnd11@users.noreply.github.com>
2024-06-24 09:48:11 -0300

    Add files via upload

---
+++ ParImpar/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace parImpar;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int result = 11 % 2;
+
+        Console.WriteLine(result);
+
+        if (result == 0)
+        {
+            Console.WriteLine("par");
+        }
+        else
+        {
+            Console.WriteLine("impar");
+        }
+    }
+}
